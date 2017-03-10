54777b35273bd3cb96966e859c7908a319f05410
Austin Truex <austintruex68@gmail.com>
2017-03-10 13:36:26 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'];
+
+
+gulp.task('serve', function() {
+    var options = {
+        script: 'server.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting....');
+        });
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+//app.use is ran by express before anything else this one sets up a static directory ie: look in these directories before serving the routes
+app.use(express.static('public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'test'
+    });
+});
+
+
+app.listen(port, function(err) {
+    console.log('running on server port: ' + port);
+});
